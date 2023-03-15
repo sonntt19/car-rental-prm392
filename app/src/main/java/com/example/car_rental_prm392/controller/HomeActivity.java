19e868466753e01a0f97c088f37d607ac78bfec1
ducnhmhe151166 <ducnhmhe151166@fpt.edu.vn>
2023-03-15 19:58:55 +0700

    create and edit home layout

--- app/src/main/java/com/example/car_rental_prm392/controller/HomeActivity.java
+++ app/src/main/java/com/example/car_rental_prm392/controller/HomeActivity.java
@@ -1,15 +1,31 @@
 package com.example.car_rental_prm392.controller;
 
-import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+
+import android.os.Bundle;
 
 import com.example.car_rental_prm392.R;
 
+import java.util.ArrayList;
+import java.util.Locale;
+
 public class HomeActivity extends AppCompatActivity {
+    RecyclerView.Adapter adapter;
+    RecyclerView rvCategory;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    void recycleViewCategory(){
+        LinearLayoutManager llm = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        rvCategory = findViewById(R.id.recyclerView);
+        rvCategory.setLayoutManager(llm);
+
     }
 }
