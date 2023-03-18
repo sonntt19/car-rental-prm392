8f256fde002400986032c776e5b72f4b4240ebfb
ducnhmhe151166 <ducnhmhe151166@fpt.edu.vn>
2023-03-18 17:19:35 +0700

    create history item, historydetail

---
+++ app/src/main/java/com/example/car_rental_prm392/controller/common/DetailHistoryActivity.java
@@ -0,0 +1,16 @@
+package com.example.car_rental_prm392.controller.common;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.car_rental_prm392.R;
+
+public class DetailHistoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_history);
+    }
+}
